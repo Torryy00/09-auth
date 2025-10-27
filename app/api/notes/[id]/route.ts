0ed17138b8acc0a4b2faf302026293c9c68c32bf
Torryy00 <votakmasha@gmail.com>
2025-10-27 18:53:45 +0100

    commot

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,27 +1,81 @@
-export type NoteTag = "Work" | "Personal" | "Meeting" | "Shopping" | "Todo";
+import { NextResponse } from 'next/server';
+import { api } from '../../api';
+import { cookies } from 'next/headers';
+import { logErrorResponse } from '../../_utils/utils';
+import { isAxiosError } from 'axios';
 
-export const TAGS = ["All", "Work", "Todo", "Personal", "Meeting", "Shopping"];
+type Props = {
+  params: Promise<{ id: string }>;
+};
 
-export interface TagsProps {
-  activeTag: string;
-  setActiveTag: (tag: string) => void;
+export async function GET(request: Request, { params }: Props) {
+  try {
+    const cookieStore = await cookies();
+    const { id } = await params;
+    const res = await api(`/notes/${id}`, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
 }
 
-export interface SidebarTagsProps {
-  activeTag: string;
+export async function DELETE(request: Request, { params }: Props) {
+  try {
+    const cookieStore = await cookies();
+    const { id } = await params;
+
+    const res = await api.delete(`/notes/${id}`, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
 }
 
-export interface Note {
-    id: string;
-    title: string;
-    content: string;
-    createdAt: string; 
-    updatedAt: string;
-    tag: NoteTag;
-  }
+export async function PATCH(request: Request, { params }: Props) {
+  try {
+    const cookieStore = await cookies();
+    const { id } = await params;
+    const body = await request.json();
 
-  export interface CreateNoteParams {
-    title: string;
-    content: string;
-    tag: NoteTag;
+    const res = await api.patch(`/notes/${id}`, body, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      logErrorResponse(error.response?.data);
+      return NextResponse.json(
+        { error: error.message, response: error.response?.data },
+        { status: error.status }
+      );
+    }
+    logErrorResponse({ message: (error as Error).message });
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
+}
